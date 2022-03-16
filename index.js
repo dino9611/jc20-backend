1a6c69ee6abc24889bf25a66e260ac8ebd26ac55
dino9611 <aldinorahman36@gmail.com>
2022-03-16 12:16:36 +0700

    init

---
+++ index.js
@@ -0,0 +1,77 @@
+// cara import di commonJs atau dibawah es6
+const http = require("http");
+// const fs = require("fs");
+// const fs = require("fs/promises");
+const {
+  readFileSync,
+  writeFileSync,
+  appendFileSync,
+  writeFile,
+  appendFile,
+  existsSync,
+  mkdirSync,
+  readFile,
+} = require("fs");
+const path = require("path");
+const PORT = 4000;
+
+let database = [
+  {
+    id: 1,
+    username: "dino",
+    password: "strong9",
+  },
+];
+
+const server = http.createServer(async (req, res) => {
+  // console.log(req); // coba lihat yaa diterminal
+  // console.log(req.url)
+
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    console.log(__dirname);
+    // path resolve
+    console.log(path.resolve(__dirname, "./index.html"));
+    let index = await fs.readFile(
+      path.resolve(__dirname, "./index.html"),
+      "utf-8"
+    );
+    // console.log(index);
+    res.end(index);
+  } else if (req.url === "/user" && req.method === "GET") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(JSON.stringify(database));
+  } else if (req.url === "/file" && req.method === "POST") {
+    // ambil data dari req.body
+    let data = ``;
+    // trigger saat data masih loading
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    // data setelah loading
+    req.on("end", () => {
+      let newdata = JSON.parse(data);
+      if (!existsSync("./files")) {
+        mkdirSync("./files");
+      }
+      writeFile(
+        `./files/lat${new Date().getTime()}.txt`,
+        newdata.kata,
+        (err) => {
+          if (err) {
+            console.log(err, "TES LINE 50");
+            res.writeHead(500, { "Content-type": "application/json" });
+            return res.end({ message: err });
+          }
+          res.writeHead(200, { "Content-type": "application/json" });
+          return res.end(JSON.stringify({ message: "berhasil" }));
+        }
+      );
+    });
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1>NotFound</h1>");
+  }
+});
+
+server.listen(PORT, () => console.log("server jalan di port " + PORT));
